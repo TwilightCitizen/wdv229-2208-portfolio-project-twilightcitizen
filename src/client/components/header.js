43e9a95c3e919dc3daba5dbe187633357bdef154
twilightcitizen <twilightcitizen@icloud.com>
2022-08-12 12:41:19 -0400

    Modified header.js to accept a backNavigation object through PageContext to display a back navigation link, if any, to the page the current page sets.

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 
 import { ColorContext, LayoutContext, PageContext } from "../app/App";
+import { NavLink } from "react-router-dom";
+import { RiArrowGoBackFill } from "react-icons/ri";
 
 const Header = () => {
     const colors = useContext(ColorContext)
@@ -8,10 +10,22 @@ const Header = () => {
     const [page,] = useContext(PageContext);
     const icon = page.icon ? page.icon(styles.icon) : <></>;
 
+    const backNavigation = (backNavigation => {
+        if (!backNavigation) return;
+
+        return (
+            <NavLink to={backNavigation.link} title={backNavigation.title} style={styles.backNavigation}>
+                <RiArrowGoBackFill style={styles.backNavigationArrow(colors)}/>
+                <p style={styles.backNavigationLabel(colors)}>Go back to {backNavigation.title}</p>
+            </NavLink>
+        )
+    })(page.backNavigation);
+
     return (
         <header style={styles.header(layout.header, colors)}>
             {icon}
             <h1>{page.title}</h1>
+            {backNavigation}
         </header>
     );
 };
@@ -19,20 +33,41 @@ const Header = () => {
 export default Header;
 
 const styles = {
+    header: (layout, colors) => ({
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "start",
+
+        ...layout,
+
+        background: colors.dark,
+        color: colors.veryLight
+    }),
+
     icon: {
         height: "0.75in",
         margin: "0.25in",
         width: "auto"
     },
 
-    header: (layout, colors) => ({
+    backNavigation: {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
+        gap: "0.0625in",
+        marginLeft: "auto",
+        marginRight: "0.25in",
+        textDecoration: "none"
+    },
 
-        ...layout,
+    backNavigationArrow: colors => ({
+        color: colors.veryLight,
+    }),
 
-        background: colors.dark,
-        color: colors.veryLight
+    backNavigationLabel: colors => ({
+        color: colors.veryLight,
+        fontWeight: "bold",
+
     })
 };
