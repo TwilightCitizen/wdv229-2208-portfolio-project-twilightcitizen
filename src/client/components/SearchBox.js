f02bfe8f0e5654e9fbe5000f1f001d6df94d3ea2
twilightcitizen <twilightcitizen@icloud.com>
2022-08-12 14:02:45 -0400

    Added additional styling to SearchBox.

--- src/client/components/SearchBox.js
+++ src/client/components/SearchBox.js
@@ -1,10 +1,59 @@
+import { RiSearchFill, RiArrowRightCircleFill } from "react-icons/ri";
+import { useContext } from "react";
+
+import { ColorContext } from "../app/App";
+
 const SearchBox = props => {
+    const colors = useContext(ColorContext);
+
     return (
-        <form style={props.style}>
-            <label htmlFor={"search"}>Search</label>
-            <input id={"search"} name={"search"} type={"text"}/>
-        </form>
+        <div style={{...props.style, ...styles.search(colors)}}>
+            <RiSearchFill style={styles.icon(colors)}/>
+
+            <label htmlFor={"search"} style={styles.label}>Search</label>
+            <input id={"search"} name={"search"} type={"text"} placeholder={"Search for Groups"} style={styles.field}/>
+
+            <label htmlFor={"button"} style={styles.label}>Go</label>
+
+            <button id={"button"} name={"button"} style={styles.button(colors)}>
+                <RiArrowRightCircleFill style={styles.icon(colors)}/>
+            </button>
+        </div>
     );
 };
+
+const styles = {
+    search: colors => ({
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        background: colors.veryLight,
+        padding: "0.0625in",
+        borderRadius: "0.03125in"
+    }),
+
+    icon: colors => ({
+        height: "0.25in",
+        width: "auto",
+        background: colors.veryLight,
+        color: colors.dark
+    }),
+
+    label: {
+        visibility: "hidden",
+        width: "0"
+    },
+
+    field: {
+        border: "none",
+        width: "3in"
+    },
+
+    button: colors => ({
+        padding: "0",
+        borderRadius: "100%",
+        background: colors.veryLight
+    })
+}
 
 export default SearchBox;
