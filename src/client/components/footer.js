4833f5479f0cc72c81db6ed6a0d5eb155eae7a7f
twilightcitizen <twilightcitizen@icloud.com>
2022-08-12 07:46:05 -0400

    Applied LayoutContext in header.js and footer.js for layout styling consistent with App.js constraints.

--- src/client/components/footer.js
+++ src/client/components/footer.js
@@ -1,7 +1,24 @@
+import { useContext } from "react";
+
+import { LayoutContext } from "../app/App";
+
 const Footer = () => {
+    const layout = useContext(LayoutContext);
+
     return (
-        <p>Hello from Footer!</p>
+        <footer style={styles.footer(layout.footer)}>
+            <p>Hello from Footer!</p>
+        </footer>
     );
 };
 
 export default Footer;
+
+const styles = {
+    footer: layout => ({
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        ...layout
+    })
+};
--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,7 +1,24 @@
+import { useContext } from "react";
+
+import { LayoutContext } from "../app/App";
+
 const Header = () => {
+    const layout = useContext(LayoutContext);
+
     return (
-        <p>Hello from Header!</p>
+        <header style={styles.header(layout.header)}>
+            <p>Hello from Header!</p>
+        </header>
     );
 };
 
 export default Header;
+
+const styles = {
+    header: layout => ({
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        ...layout
+    })
+};
