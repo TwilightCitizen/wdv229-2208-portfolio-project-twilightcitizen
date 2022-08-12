85adf7887f61362674665be6a3ed084321f1c96e
twilightcitizen <twilightcitizen@icloud.com>
2022-08-12 09:56:25 -0400

    Modified header to accept an object containing page title and function that returns an icon styled with provided style, displaying the styled icon left of the heading for the title.

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -6,10 +6,12 @@ const Header = () => {
     const colors = useContext(ColorContext)
     const layout = useContext(LayoutContext);
     const [page,] = useContext(PageContext);
+    const icon = page.icon ? page.icon(styles.icon) : <></>;
 
     return (
         <header style={styles.header(layout.header, colors)}>
-            <h1>{page}</h1>
+            {icon}
+            <h1>{page.title}</h1>
         </header>
     );
 };
@@ -17,6 +19,12 @@ const Header = () => {
 export default Header;
 
 const styles = {
+    icon: {
+        height: "0.75in",
+        margin: "0.25in",
+        width: "auto"
+    },
+
     header: (layout, colors) => ({
         display: "flex",
         flexDirection: "row",
