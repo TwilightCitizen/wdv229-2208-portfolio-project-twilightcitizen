4286069ebc8a5b750e81edb20ede7938f7f81af6
twilightcitizen <twilightcitizen@icloud.com>
2022-08-12 07:44:20 -0400

    Implemented LayoutContext in App.js to provide layout constraints to subordinate pages and components.

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -4,6 +4,8 @@ import {
     Route
 } from "react-router-dom";
 
+import { createContext } from "react";
+
 import Header from "../components/header";
 import Footer from "../components/footer";
 
@@ -12,31 +14,56 @@ import Detail from "../pages/detail";
 import User from "../pages/user"
 import Search from "../pages/search"
 
+const LayoutContext = createContext({});
+
 const App = () => {
     return (
         <div className="App" style={styles.app}>
-            <Header/>
+            <LayoutContext.Provider value={layout}>
+                <Header/>
 
-            <Routes>
-                <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
-                <Route path="/dashboard" element={<Dashboard/>}/>
-                <Route path="/detail" element={<Detail/>}/>
-                <Route path="/user" element={<User/>}/>
-                <Route path="/search" element={<Search/>}/>
-            </Routes>
+                <div style={styles.content}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
+                        <Route path="/dashboard" element={<Dashboard/>}/>
+                        <Route path="/detail" element={<Detail/>}/>
+                        <Route path="/user" element={<User/>}/>
+                        <Route path="/search" element={<Search/>}/>
+                    </Routes>
+                </div>
 
-            <Footer/>
+                <Footer/>
+            </LayoutContext.Provider>
         </div>
     );
 }
 
 export default App;
 
+export { LayoutContext };
+
+const layout = {
+    app: {
+        minHeight: "100vh"
+    },
+
+    header: {
+        height: "1.25in"
+    },
+
+    footer: {
+        height: "0.25in"
+    }
+};
+
 const styles = {
     app: {
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        flexWrap: 'wrap'
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap"
+    },
+
+    content: {
+        minHeight: `calc(${layout.app.minHeight} - ${layout.header.height} - ${layout.footer.height})`
     }
-}
+};
