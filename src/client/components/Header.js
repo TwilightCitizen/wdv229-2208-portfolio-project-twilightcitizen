9a6818cdf90d0dc08de6703e0066320cfffc416b
twilightcitizen <twilightcitizen@icloud.com>
2022-08-13 14:16:09 -0400

    Factored back navigation functionality out of Header.js into a component, IconNavLink.js, with additional functionality to style based on hover and focus states, which Header.js now uses.

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -8,11 +8,15 @@ Portfolio Project
 // Imports
 
 import { useContext } from "react";
-import { NavLink } from "react-router-dom";
 import { RiArrowGoBackFill } from "react-icons/ri";
 
 import { ColorContext, LayoutContext, PageContext } from "../app/App";
 import SearchBox from "./SearchBox"
+import IconNavLink from "./IconNavLink";
+
+// Constants
+
+const backNavigationIcon = style => <RiArrowGoBackFill style={style}/>
 
 // Component
 
@@ -22,23 +26,19 @@ const Header = () => {
     const [page,] = useContext(PageContext);
     const icon = page.icon ? page.icon(styles.icon) : <></>;
 
-    const backNavigation = backNavigation => {
-        if (!backNavigation) return;
-
-        return (
-            <NavLink to={backNavigation.link} title={backNavigation.title} style={styles.backNavigation}>
-                <RiArrowGoBackFill style={styles.backNavigationArrow(colors)}/>
-                <p style={styles.backNavigationLabel(colors)}>Go back to {backNavigation.title}</p>
-            </NavLink>
-        )
-    };
-
     return (
         <header style={styles.header(layout.header, colors)}>
             {icon}
             <h1>{page.title}</h1>
             {page.showSearch ? <SearchBox style={styles.search}/> : null}
-            {backNavigation(page.backNavigation)}
+
+            {page.backNavigation ?
+                <IconNavLink
+                    link={page.backNavigation.link}
+                    label={`Back to ${page.backNavigation.title}`}
+                    icon={backNavigationIcon}
+                /> : null
+            }
         </header>
     );
 };
@@ -71,25 +71,5 @@ const styles = {
     search: {
         marginLeft: "auto",
         marginRight: "0.25in",
-    },
-
-    backNavigation: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        gap: "0.0625in",
-        marginLeft: "auto",
-        marginRight: "0.25in",
-        textDecoration: "none"
-    },
-
-    backNavigationArrow: colors => ({
-        color: colors.veryLight,
-    }),
-
-    backNavigationLabel: colors => ({
-        color: colors.veryLight,
-        fontWeight: "bold",
-
-    })
+    }
 };
