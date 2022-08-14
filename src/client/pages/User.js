e2933d630003ac1189c02897318be4deef10539c
twilightcitizen <twilightcitizen@icloud.com>
2022-08-14 16:36:17 -0400

    Modified User.js to use LayoutContext for page layout styling consistent with other pages, ColorContext for color styling of child elements, and UserContext for displaying details of selected user.

--- src/client/pages/User.js
+++ src/client/pages/User.js
@@ -10,7 +10,13 @@ Portfolio Project
 import { useContext, useEffect } from "react";
 import { RiUserFill } from "react-icons/ri";
 
-import { PageContext, ChatContext } from "../app/App";
+import {
+    PageContext,
+    ChatContext,
+    LayoutContext,
+    UserContext,
+    ColorContext
+} from "../app/App";
 
 // Constants
 
@@ -21,6 +27,9 @@ const icon = style => <RiUserFill style={style}/>
 const User = () => {
     const [, setPage] = useContext(PageContext);
     const [chat, setChat] = useContext(ChatContext);
+    const layout = useContext(LayoutContext)
+    const [user,] = useContext(UserContext)
+    const colors = useContext(ColorContext)
 
     useEffect(() => {
         setPage(() => ({
@@ -37,7 +46,33 @@ const User = () => {
     }, [setPage, chat, setChat]);
 
     return (
-        <p>Hello from User!</p>
+        <div style={styles.user(layout.page)}>
+            <RiUserFill style={styles.profilePic(colors)}/>
+
+            <label htmlFor={"username"}>Username</label>
+
+            <input
+                id={"username"} name={"username"}
+                type={"text"} value={user.username}
+                disabled={true} style={styles.input(colors)}
+            />
+
+            <label htmlFor={"displayName"}>Display Name</label>
+
+            <input
+                id={"displayName"} name={"displayName"}
+                type={"text"} value={user.displayName}
+                disabled={true} style={styles.input(colors)}
+            />
+
+            <label htmlFor={"daysOnKik"}>Days on Kik</label>
+
+            <input
+                id={"daysOnKik"} name={"daysOnKik"}
+                type={"text"} value={"123"}
+                disabled={true} style={styles.input(colors)}
+            />
+        </div>
     );
 };
 
@@ -46,3 +81,33 @@ const User = () => {
 export default User;
 
 // Styles
+
+const styles = {
+    user: layout => ({
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        gap: "0.0625in",
+
+        ...layout
+    }),
+
+    profilePic: colors => ({
+        height: "3in",
+        width: "3in",
+        borderRadius: "100%",
+        background: colors.dark,
+        color: colors.veryLight
+    }),
+
+    input: colors => ({
+        fontSize: "1.5em",
+        fontWeight: "bold",
+        color: colors.veryDark,
+        background: colors.light,
+        borderRadius: "0.0625in",
+        padding: "0.0625in",
+        border: `1px solid ${colors.dark}`,
+        width: "3in"
+    })
+};
