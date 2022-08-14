38e58fdf2d3f41b9dd2bcc3ab33602092acc5c36
twilightcitizen <twilightcitizen@icloud.com>
2022-08-14 13:27:29 -0400

    Modified Search.js to use LayoutContext for page styling consistent with the rest of the app.  Added mock data for groups which are filtered and displayed when containing the search terms.

--- src/client/pages/Search.js
+++ src/client/pages/Search.js
@@ -10,7 +10,7 @@ Portfolio Project
 import { useContext, useEffect } from "react";
 import { RiSearchFill } from "react-icons/ri";
 
-import { PageContext, SearchContext } from "../app/App";
+import { LayoutContext, PageContext, SearchContext } from "../app/App";
 
 // Constants
 
@@ -21,7 +21,12 @@ const icon = style => <RiSearchFill style={style}/>
 const Search = () => {
     const [, setPage] = useContext(PageContext);
     const [search, setSearch] = useContext(SearchContext);
+    const layout = useContext(LayoutContext)
     const searchTerms = search;
+
+    const searchResults = groups
+        .filter(group => group.displayName.toLowerCase().includes(searchTerms.toLowerCase()))
+        .map((group, index) => <p key={index}>{group.displayName}</p>);
 
     useEffect(() => {
         setPage(() => ({
@@ -38,9 +43,15 @@ const Search = () => {
     }, [setPage]);
 
     return (
-        <>
-            <p>You searched for {searchTerms}</p>
-        </>
+        <div style={styles.search(layout.page)}>
+            {
+                searchResults.length ?
+                <h2>Here are groups matching "{searchTerms}":</h2> :
+                <h2>There are no groups matching "{searchTerms}".</h2>
+            }
+
+            {searchResults}
+        </div>
     );
 };
 
@@ -49,3 +60,55 @@ const Search = () => {
 export default Search;
 
 // Styles
+
+const styles = {
+    search: layout => ({
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+
+        ...layout
+    })
+};
+
+// Mock Data
+
+const groups = [
+    {
+        jid: "1234567891_g@groups.kik.com",
+        displayName: "Cats"
+    }, {
+        jid: "1234567892_g@groups.kik.com",
+        displayName: "Cats are Great"
+    }, {
+        jid: "1234567893_g@groups.kik.com",
+        displayName: "We Hate Cats"
+    },  {
+        jid: "1234567894_g@groups.kik.com",
+        displayName: "Cat Lovers"
+    }, {
+        jid: "1234567891_g@groups.kik.com",
+        displayName: "Dogs"
+    }, {
+        jid: "1234567892_g@groups.kik.com",
+        displayName: "Dogs are Great"
+    }, {
+        jid: "1234567893_g@groups.kik.com",
+        displayName: "We Hate Dogs"
+    },  {
+        jid: "1234567894_g@groups.kik.com",
+        displayName: "Dog Lovers"
+    }, {
+        jid: "1234567891_g@groups.kik.com",
+        displayName: "Bunnies"
+    }, {
+        jid: "1234567892_g@groups.kik.com",
+        displayName: "Bunnies are Great"
+    }, {
+        jid: "1234567893_g@groups.kik.com",
+        displayName: "We Hate Bunnies"
+    },  {
+        jid: "1234567894_g@groups.kik.com",
+        displayName: "Bunny Lovers"
+    },
+];
