9b2e932ad6e15f1e329ef5d17a1e17f3489fce48
twilightcitizen <twilightcitizen@icloud.com>
2022-08-20 15:28:44 -0400

    Modified detail, using React Async fetch to retrieve chat events for the selected group or private chat, and displaying content conditional on the results retrieved. Removed mock data.

--- src/client/pages/Detail.js
+++ src/client/pages/Detail.js
@@ -8,6 +8,7 @@ Portfolio Project
 // Imports
 
 import { useContext, useEffect } from "react";
+import { useFetch } from "react-async";
 import { RiChat4Fill } from "react-icons/ri";
 
 import { PageContext, ChatContext, LayoutContext } from "../app/App";
@@ -16,6 +17,8 @@ import ChatDetail from "../components/ChatDetail";
 // Constants
 
 const icon = style => <RiChat4Fill style={style}/>;
+const serverPort = process.env.REACT_APP_SERVER_PORT;
+const url = id => `http://localhost:${serverPort}/details/${id}`;
 
 // Component
 
@@ -24,12 +27,15 @@ const Detail = () => {
     const [chat, setChat] = useContext(ChatContext);
     const layout = useContext(LayoutContext);
     const selectedChat = chat;
-    const isGroup = selectedChat.jid.split("@")[1].split(".")[0] === "groups";
 
-    const chatDetails = (isGroup ? groupChatEvents : privateChatEvents(selectedChat))
-        .map((chatDetail, index) =>
-            <ChatDetail detail={chatDetail} chat={selectedChat} key={index}/>
-        );
+    console.log(selectedChat._id);
+
+    const isGroup = selectedChat._id.split("@")[1] === "groups.kik.com";
+    const headerRest = `${isGroup ? "in" : "with"} ${selectedChat.displayName}`;
+
+    const { data: chatDetails, error: chatDetailsError, isPending: chatDetailsPending } = useFetch(
+        url(selectedChat._id), { headers: { accept: "application/json" } }
+    );
 
     useEffect(() => {
         setPage(() => ({
@@ -47,8 +53,17 @@ const Detail = () => {
 
     return (
         <div style={styles.detail(layout.page)}>
-            <h2>Chat Details {isGroup ? "in" : "with"} {selectedChat.displayName}</h2>
-            {chatDetails}
+            {
+                chatDetailsPending ?
+                    <h2>Searching for Chat Details {headerRest}</h2> :
+                chatDetailsError ?
+                    <h2>Error Searching for Chat Details {headerRest}</h2> :
+                    <h2>Chat Details {headerRest}</h2>
+            } {
+                chatDetails ? chatDetails.map((chatDetail, index) =>
+                    <ChatDetail detail={chatDetail} chat={selectedChat} key={index}/>
+                ) : null
+            }
         </div>
     );
 };
@@ -69,69 +84,3 @@ const styles = {
         ...layout
     })
 };
-
-// Mock Data
-
-const groupChatUser = {
-    jid: "fakeuser1@talk.kik.com",
-    username: "fakeuser1",
-    displayName: "Fake User 1"
-};
-
-const groupChatEvents = [
-    {
-        user: groupChatUser,
-        timeStamp: "2022-08-14, 14:08",
-        event: "join",
-        content: ""
-    }, {
-        user: groupChatUser,
-        timeStamp: "2022-08-14, 14:08",
-        event: "chat",
-        content: "Hey, group!  How goes it?"
-    }, {
-        user: groupChatUser,
-        timeStamp: "2022-08-14, 14:12",
-        event: "image",
-        content: "http://link.to.image.png"
-    }, {
-        user: groupChatUser,
-        timeStamp: "2022-08-14, 14:13",
-        event: "chat",
-        content: "Sure is quiet in here."
-    }, {
-        user: groupChatUser,
-        timeStamp: "2022-08-14, 14:14",
-        event: "gif",
-        content: "http://link.to.gif.mp4"
-    }, {
-        user: groupChatUser,
-        timeStamp: "2022-08-14, 14:08",
-        event: "leave",
-        content: ""
-    }
-];
-
-const privateChatEvents = privateChatUser => ([
-    {
-        user: privateChatUser,
-        timeStamp: "2022-08-14, 14:08",
-        event: "chat",
-        content: "Hey, bot!  How goes it?"
-    }, {
-        user: privateChatUser,
-        timeStamp: "2022-08-14, 14:09",
-        event: "chat",
-        content: "How come you never say anything?"
-    }, {
-        user: privateChatUser,
-        timeStamp: "2022-08-14, 14:10",
-        event: "image",
-        content: "http://link.to.image.png"
-    }, {
-        user: groupChatUser,
-        timeStamp: "2022-08-14, 14:11",
-        event: "gif",
-        content: "http://link.to.gif.mp4"
-    }
-]);
