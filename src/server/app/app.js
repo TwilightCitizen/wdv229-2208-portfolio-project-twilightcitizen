00aa30fbd74ee2cae0c285f31f7a2733542b20bf
twilightcitizen <twilightcitizen@icloud.com>
2022-08-20 13:45:02 -0400

    Added allow headers back to server app CORS policy middleware.

--- src/server/app/app.js
+++ src/server/app/app.js
@@ -39,6 +39,12 @@ app.use(express.json());
 
 app.use((request, response, next) => {
     response.header("Access-Control-Allow-Origin", "*");
+
+    response.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
     next();
 });
 
